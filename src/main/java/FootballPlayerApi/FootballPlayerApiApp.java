a6b41fbd276852c6ad36ed748e44a717178b1959
Md. Samim Akhter <srsamim043@gmail.com>
2022-09-07 15:53:56 +0600

    Added the main run file

--- src/main/java/FootballPlayerApi/FootballPlayerApiApp.java
+++ src/main/java/FootballPlayerApi/FootballPlayerApiApp.java
@@ -1,2 +1,12 @@
-package FootballPlayerApi;public class FootballPlayerApiApp {
+package FootballPlayerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPlayerApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FootballPlayerApiApp.class, args);
+
+    }
 }
